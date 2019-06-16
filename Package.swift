3720d350d1b97d6bddcd60bcdbdeac08f26f258d
Robert Pugh <aquilosion@icloud.com>
2019-06-16 20:28:21 +0100

    Update Swift package

--- Package.swift
+++ Package.swift
@@ -1,6 +1,19 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
-    name: "Freddy"
+    name: "Freddy",
+    products: [
+        .library(
+            name: "Freddy",
+            targets: ["Freddy"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "Freddy",
+            dependencies: []),
+    ]
 )
-
